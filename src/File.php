cfdb98552ff26c6508ac6afd2c4824dd521f4009
Jeremy Angele <jeremy.angele@proton.me>
2023-07-30 00:21:56 +0200

    Add insider binary

--- src/File.php
+++ src/File.php
@@ -3,6 +3,8 @@
 namespace Angelej\PhpInsider;
 
 use SplFileObject;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 
 /**
  * @mixin \SplFileObject
@@ -28,6 +30,49 @@ class File {
     public function getContent(): string {
 
         return $this->fread($this->getSize());
+    }
+
+
+    /**
+     * @param  string $location
+     * @param  array $extensions
+     * @param  array $excludedLocations
+     * @return array|\Angelej\PhpInsider\File[]
+     */
+    public static function glob(string $location, array $extensions = ['php'], array $excludedLocations = []): array {
+
+        $files = [];
+        $excludedPaths = [];
+        array_map(function($excludedPath) use (&$excludedPaths){
+
+            if(is_array($paths = glob($excludedPath))){
+
+                foreach($paths as $path){
+                    $excludedPaths[] = realpath($path);
+                }
+            }
+        }, $excludedLocations);
+
+        $dirIterator = new RecursiveDirectoryIterator($location);
+
+        foreach(new RecursiveIteratorIterator($dirIterator) as $file){
+
+            // filter directories and symlinks
+            if($file->isDir() || $file->isLink()) continue;
+
+            // filter file extensions
+            if(!in_array(strtolower($file->getExtension()), $extensions)) continue;
+
+            $realPath = $file->getRealPath();
+
+            // filter excluded paths
+            foreach($excludedPaths as $excludedPath){
+
+                if(str_starts_with($realPath, $excludedPath)) continue 2;
+            }
+            $files[] = new File($realPath);
+        }
+        return $files;
     }
 
     /**
