ca71c04ac4455b5669249c617e9a92845d87c24a
Jeremy Angele <131715596+angelej@users.noreply.github.com>
2025-12-28 19:02:21 +0100

    test: binary --format=json option

--- tests/Feature/BinaryTest.php
+++ tests/Feature/BinaryTest.php
@@ -13,3 +13,13 @@ it('executes binary', function () {
         ->toBeInt()
         ->toBe(1);
 });
+
+it('outputs json', function () {
+    exec('./bin/insider analyse tests/Unit/files/EvalFile.php --format=json', $output, $resultCode);
+    expect($resultCode)
+        ->toBeInt()
+        ->toBe(1);
+    $output = implode("\n", $output);
+    expect(json_validate($output))
+        ->toBeTrue();
+});
